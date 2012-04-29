29fbdca38ec2db16b1f09306af869fce1f085fed
erenon <erenon2@gmail.com>
2012-04-29 22:27:24 +0200

    build script: load every species

--- bin/build_database.sh
+++ bin/build_database.sh
@@ -5,21 +5,6 @@
 dir="$( cd "$( dirname "$0" )" && pwd )"
 console=$dir"/../app/console --env=test"
 
-#echo "Generatre entities"
-#php $console comppi:load:entities
-
-#echo "Add set/get methods..."
-#php $console doctrine:generate:entities LoaderBundle --no-backup
-
-#echo "Drop database schema"
-#php $console doctrine:schema:drop --force
-
-#echo "Create new schema"
-#php $console doctrine:schema:update --force
-
-#echo "Load plaintext databases"
-#php $console comppi:load:database
-
 echo "Drop database schema"
 php $console doctrine:schema:drop --force
 
@@ -34,3 +19,31 @@ php $console comppi:build:interactions sc
 
 echo "Load SacCe localizations"
 php $console comppi:build:localizations sc
+
+echo "Load Human maps"
+php $console comppi:build:map hs
+
+echo "Load Human interactions"
+php $console comppi:build:interactions hs
+
+echo "Load Human localizations"
+php $console comppi:build:localizations hs
+
+echo "Load Drosi maps"
+php $console comppi:build:map dm
+
+echo "Load Drosi interactions"
+php $console comppi:build:interactions dm
+
+echo "Load Drosi localizations"
+php $console comppi:build:localizations dm
+
+echo "Load C'Elegans maps"
+php $console comppi:build:map ce
+
+echo "Load C'Elegans interactions"
+php $console comppi:build:interactions ce
+
+echo "Load C'Elegans localizations"
+php $console comppi:build:localizations ce
+
