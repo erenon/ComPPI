139163df2e205ffa29a1841d1b20a1cb9b577ae4
erenon <erenon2@gmail.com>
2012-05-21 18:29:53 +0200

    Intact: disallow inter-specie interaction, read uniprot names only

--- src/Comppi/BuildBundle/Service/DatabaseProvider/Parser/Interaction/Intact.php
+++ src/Comppi/BuildBundle/Service/DatabaseProvider/Parser/Interaction/Intact.php
@@ -20,36 +20,66 @@ class Intact extends AbstractInteractionParser
     protected $headerCount = 0;
 
     protected function readRecord() {
-        $line = $this->readLine();
+        $validRead = false;
 
-        if ($line === false) {
-            // EOF
-            return;
+        while (!$validRead) {
+            $line = $this->readLine();
+
+            if ($line === false) {
+                // EOF
+                return;
+            }
+
+            $recordArray = explode("\t", $line);
+            $this->checkRecordFieldCount($recordArray, 31);
+
+            // check for same taxid:
+            if ($recordArray[9] !== $recordArray[10]) {
+                $validRead = false;
+                continue;
+            } else {
+                $validRead = true;
+            }
+
+            // 10: strlen('uniprotkb:')
+            $proteinAName = $this->getProteinFromField($recordArray[0]);
+            $proteinBName = $this->getProteinFromField($recordArray[1]);
+
+            if ($proteinAName === false || $proteinBName === false) {
+                $validRead = false;
+                continue;
+            } else {
+                $validRead = true;
+            }
+
+            // 7: strlen('pubmed:')
+            $pubmedId = substr($recordArray[8], 7);
+
+            // extract experimental system type
+            $expSysStart = strpos($recordArray[6], '(') + 1;
+            $expSysEnd = strpos($recordArray[6], ')');
+
+            $expSysType = substr($recordArray[6], $expSysStart, $expSysEnd-$expSysStart);
+
+            $this->currentRecord = array(
+                'proteinANamingConvention' => 'UniProtKB-AC',
+                'proteinAName' => $proteinAName,
+                'proteinBNamingConvention' => 'UniProtKB-AC',
+                'proteinBName' => $proteinBName,
+                'pubmedId' => $pubmedId,
+                'experimentalSystemType' => $expSysType
+            );
+        }
+    }
+
+    protected function getProteinFromField($field) {
+        $names = explode('|', $field);
+        foreach ($names as $name) {
+            if (substr($name, 0, 10) == 'uniprotkb:') {
+                return substr($name, 10);
+            }
         }
 
-        $recordArray = explode("\t", $line);
-        $this->checkRecordFieldCount($recordArray, 31);
-
-        // 10: strlen('uniprotkb:')
-        $proteinAName = substr($recordArray[0], 10);
-        $proteinBName = substr($recordArray[1], 10);
-
-        // 7: strlen('pubmed:')
-        $pubmedId = substr($recordArray[8], 7);
-
-        // extract experimental system type
-        $expSysStart = strpos($recordArray[6], '(') + 1;
-        $expSysEnd = strpos($recordArray[6], ')');
-
-        $expSysType = substr($recordArray[6], $expSysStart, $expSysEnd-$expSysStart);
-
-        $this->currentRecord = array(
-            'proteinANamingConvention' => 'UniProtKB-AC',
-            'proteinAName' => $proteinAName,
-            'proteinBNamingConvention' => 'UniProtKB-AC',
-            'proteinBName' => $proteinBName,
-            'pubmedId' => $pubmedId,
-            'experimentalSystemType' => $expSysType
-        );
+        return false;
     }
 }
