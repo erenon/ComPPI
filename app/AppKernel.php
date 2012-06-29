4917d0e3cecd2273252cfb4166f172112cab3b13
erenon <erenon2@gmail.com>
2012-06-29 23:30:13 +0200

    DownloadBundle removed

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            new Comppi\DownloadBundle\DownloadBundle(),
             new Comppi\BuildBundle\BuildBundle(),
             new Comppi\StatBundle\StatBundle(),
         );
--- src/Comppi/DownloadBundle/DownloadBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Comppi\DownloadBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class DownloadBundle extends Bundle
-{
-}
