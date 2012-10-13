ace0f561ed789039bea9c692187d41a18b1be844
erenon <erenon2@gmail.com>
2012-10-13 17:31:20 +0200

    BuildBundle: Indices added to localization tables

--- src/Comppi/BuildBundle/Entity/ProteinToLocalizationCe.php
+++ src/Comppi/BuildBundle/Entity/ProteinToLocalizationCe.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
+ * @ORM\Table(indexes={@ORM\index(name="search_idx", columns={"proteinId"})})
  */
 class ProteinToLocalizationCe
 {
@@ -15,28 +16,28 @@ class ProteinToLocalizationCe
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
-    
+
     /**
      * @ORM\Column(type="integer")
      * @ORM\ManyToOne(targetEntity="ProteinCe")
      */
     protected $proteinId;
-    
+
     /**
      * @ORM\Column(type="integer")
      */
-    protected $localizationId;    
-    
+    protected $localizationId;
+
     /**
      * @ORM\Column(type="string", length="255")
      */
     protected $sourceDb;
-    
+
     /**
      * @ORM\Column(type="integer")
      */
     protected $pubmedId;
-    
+
     /**
      * @ORM\Column(type="string", length="255")
      */
