cb7daf6b6605380d402d040a81104b1c2b8b012c
erenon <erenon2@gmail.com>
2013-02-01 22:24:48 +0100

    BuildBundle: ComppiStandard confidence score calculator: settings.php reads parameter.ini directly

--- src/Comppi/BuildBundle/Service/ConfidenceScore/Calculator/settings.php
+++ src/Comppi/BuildBundle/Service/ConfidenceScore/Calculator/settings.php
@@ -2,10 +2,6 @@
 //USER VARIABLES
 global $db_name,$db_pass,$db_host,$db_user,$nagylocID_file,$avgfile,$fi_int_ext,$fi_int_pred,$fi_int_unknown,$loctreetextile,$fi_loc_exp,$fi_loc_pred,$fi_loc_unknown,$npredpow,$confidence_type_unknown,$confidence_type_predicted,$confidence_type_experimental,$sql,$verbose;
 
-$db_name="comppi";
-$db_pass="comppi_pw";
-$db_host="localhost";
-$db_user="comppi";
 $nagylocID_file="src/Comppi/BuildBundle/Service/ConfidenceScore/Calculator/largelocIDs.txt";
 $avgfile="src/Comppi/BuildBundle/Service/ConfidenceScore/Calculator/average_scores.txt";
 $fi_int_exp=1;
@@ -20,6 +16,22 @@ $confidence_type_unknown=0;
 $confidence_type_predicted=1;
 $confidence_type_experimental=2;
 //USER VARIABLES END HERE
+
+// Hack to get db credentials
+global $kernel;
+$kernelRoot = $kernel->getRootDir(); // $kernel defined in app.php or app_dev.php
+$configPath = $kernelRoot . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'parameters.ini';
+
+$config = parse_ini_file($configPath, true);
+
+$db_host = $config['parameters']['database_host'];
+$db_name = $config['parameters']['database_name'];
+$db_user = $config['parameters']['database_user'];
+$db_pass = $config['parameters']['database_password'];
+
+unset($config);
+unset($configPath);
+unset($kernelRoot);
 
 $sql=new mysqli("$db_host","$db_user","$db_pass","$db_name");
 if(isset($_REQUEST['verbose'])) {$verbose=$_REQUEST['verbose']; if ($verbose>=-1) echo("Verbosity level {$verbose} set.\n");}
