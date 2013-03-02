25a6de5742d96b28140f72dda64ead39a5492e94
erenon <erenon2@gmail.com>
2013-03-02 20:23:04 +0100

    StatBundle: js files use spaces instead of tabs

--- src/Comppi/StatBundle/Resources/public/js/search.js
+++ src/Comppi/StatBundle/Resources/public/js/search.js
@@ -1,22 +1,22 @@
 var initProteinSearchbars = function() {
-	var resourceUrl = $('#autocomplete-url').attr('content');
-	console.log(resourceUrl);
-	
-	$('.protein-search-input').each(function(index, searchbar) {
-		var bar = $(searchbar);
-		bar.attr('autocomplete', 'off');
-		bar.typeahead({
-			'minLength': 3,
-			'source': function(query, finishCb) {
-				$.getJSON(resourceUrl + query, function(data) {
-					finishCb(data.names);
-				});
-			}
-		});
-		bar.data('items', 10);
-	});
+    var resourceUrl = $('#autocomplete-url').attr('content');
+    console.log(resourceUrl);
+
+    $('.protein-search-input').each(function(index, searchbar) {
+        var bar = $(searchbar);
+        bar.attr('autocomplete', 'off');
+        bar.typeahead({
+            'minLength' : 3,
+            'source' : function(query, finishCb) {
+                $.getJSON(resourceUrl + query, function(data) {
+                    finishCb(data.names);
+                });
+            }
+        });
+        bar.data('items', 10);
+    });
 };
 
 $(document).ready(function() {
-	initProteinSearchbars();
+    initProteinSearchbars();
 });
