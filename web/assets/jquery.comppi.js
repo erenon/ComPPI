32505b9cbf845bc5734598d7cd94137f41d600dd
Gyurkó, M. Dávid, 'dualon <gyurko.david@e-arc.hu>
2014-06-13 21:47:06 +0200

    Descriptions: more striped tables, various minor tweaks and updates.

--- web/assets/jquery.comppi.js
+++ web/assets/jquery.comppi.js
@@ -12,32 +12,6 @@ $(document).ready(function(){
 		function() { $(this).removeClass('hovered_listline'); }
 	);
 
-	// Search / Download Advanced Parameters
-	/*$("#AdvSearchFrame").hide();
-	$("#AdvSearchBtn").click(function() {
-		$("#AdvSearchFrame").slideToggle(500);
-	});*/
-
-	// ToggleButtons
-	// @TODO: create an abstract solution for this
-	/*$(".toggleButton").click(function() {
-		var field_name = "#" + $(this).attr("id").replace("Btn", "");
-		if ( $(this).hasClass('btn_green') ) {
-			$(this).removeClass('btn_green');
-			$(field_name).attr('value', 0);
-		} else {
-			$(this).addClass('btn_green');
-			$(field_name).attr('value', 1);
-		}
-	});*/
-
-	// radio buttons
-
-	/*$("#ProteinSearchSpecies").buttonset();
-	$("#DlSetButtons").buttonset();
-	$("#DlSpeciesButtons").buttonset();
-	$("#DlLocButtons").buttonset();*/
-
 	// DOWNLOADS
 	$("input[name='fDlSet']").change(function() {
 		switch(this.value) {
@@ -59,41 +33,6 @@ $(document).ready(function(){
 		$("#DlSetHelpDisplay").html( $(this).next(".DlSetHelp").html() ).fadeIn();
 	});
 	$("input[name='fDlSet']:checked").change(); // trigger the event for first time
-
-	//$(":radio.btn").hide();
-	/*$(":radio.btn + label").addClass("btn").click(function(){
-		if ( $(this).hasClass('btn_green') ) {
-			$(this).removeClass('btn_green').prev(":radio").prop("checked", false);
-		} else {
-			$(":radio.btn_green").removeClass('btn_green').prev(":radio").prop("checked", false);
-			$(this).addClass('btn_green').prev(":radio").prop("checked", true);
-			$(this).siblings(":radio").removeClass('btn_green').prop("checked", false);
-		}
-	});*/
-	//$(":radio.btn:checked + label").addClass("btn_green");
-
-
-	/*$('#ProteinSearchForm:radio').each(function(){
-		var id = $(this).attr("id");
-		var label = $('label[for="' + id + '"]');
-
-		$(this).add(label).css("display", "none").after();
-		$(this).before('<input type="button" id="'+ id +'Btn" value="'+ $(label).text() +'" class="btn radioButton" />');
-		if ($(this).is(':checked')) {
-			$('#'+id+'Btn').addClass("btn_green");
-		}
-		$('#'+id+'Btn').on('click', function(){
-			if ($(this).hasClass('btn_green')) {
-				$(this).removeClass('btn_green');
-				$("#"+id).removeAttr("checked", "checked").attr('value', 0);
-			} else {
-				$('.radioButton').removeClass('btn_green');
-				$(':radio').removeAttr('checked').attr('value', 0);
-				$(this).addClass('btn_green');
-				$('#'+id).attr('checked', 'checked').attr('value', 1);
-			}
-		});
-	});*/
 });
 
 // search autocomplete
@@ -121,6 +60,11 @@ $(function() {
 			at: "left top"
 		}
 	});
+});
+
+// striped tables
+$(function() {
+	$("table.striped_table tr:even").addClass("striped_row");
 });
 
 // show/hide protein interaction details
